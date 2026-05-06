bd4f781ae81f822bce58e18f66c7aa0f0dcda1c9
Melanie Xia <melaniekxia@gmail.com>
2026-05-05 22:47:50 -0400

    script for importing data to hapi fhir jpa server

---
+++ scripts/import_fhir_data.sh
@@ -0,0 +1,19 @@
+FHIR_URL="http://localhost:8080/fhir"
+DATA_DIR="../data/hl7_fhir_r4_json"
+
+# Upload practitioner information
+echo "Uploading practitioner and hospital information"
+curl -X POST "$FHIR_URL" -H "Content-Type: application/fhir+json" --data @"$DATA_DIR"/hospitalInformation1777098602123.json
+curl -X POST "$FHIR_URL" -H "Content-Type: application/fhir+json" --data @"$DATA_DIR"/practitionerInformation1777098602123.json
+echo "HospitalInformation and PractitionerInformation import completed"
+
+for file in $DATA_DIR/*.json; do 
+    if [[ "$file" == *practitionerInformation* ]]; then
+        continue
+    fi
+
+    echo "Uploading $resource_type"
+    curl -X POST "$FHIR_URL" -H "Content-Type: application/fhir+json" --data @"$file"
+done
+
+echo "Data import completed"
