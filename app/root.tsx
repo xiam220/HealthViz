b5c5af7449ef6000254f81d625411e771869b3f6
Melanie Xia <melaniekxia@gmail.com>
2026-05-09 21:59:58 -0400

    root and routes corrections

--- app/root.tsx
+++ app/root.tsx
@@ -6,14 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-import Home from "./routes/home";
-
-export default function App() {
-  return (
-    <Outlet />
-  );
-}
-// import type { Route } from "./+types/root";
+import type { Route } from "./+types/root";
 // import "./app.css";
 
 // export const links: Route.LinksFunction = () => [
@@ -29,53 +22,56 @@ export default function App() {
 //   },
 // ];
 
-// export function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <head>
-//         <meta charSet="utf-8" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//         <Meta />
-//         <Links />
-//       </head>
-//       <body>
-//         {children}
-//         <ScrollRestoration />
-//         <Scripts />
-//       </body>
-//     </html>
-//   );
-// }
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
 
-// export default function App() {
-//   return <Outlet />;
-// }
+export default function App() {
+  return (
+    <Outlet />
 
-// export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-//   let message = "Oops!";
-//   let details = "An unexpected error occurred.";
-//   let stack: string | undefined;
+  );
+}
 
-//   if (isRouteErrorResponse(error)) {
-//     message = error.status === 404 ? "404" : "Error";
-//     details =
-//       error.status === 404
-//         ? "The requested page could not be found."
-//         : error.statusText || details;
-//   } else if (import.meta.env.DEV && error && error instanceof Error) {
-//     details = error.message;
-//     stack = error.stack;
-//   }
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  let message = "Oops!";
+  let details = "An unexpected error occurred.";
+  let stack: string | undefined;
 
-//   return (
-//     <main className="pt-16 p-4 container mx-auto">
-//       <h1>{message}</h1>
-//       <p>{details}</p>
-//       {stack && (
-//         <pre className="w-full p-4 overflow-x-auto">
-//           <code>{stack}</code>
-//         </pre>
-//       )}
-//     </main>
-//   );
-// }
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? "404" : "Error";
+    details =
+      error.status === 404
+        ? "The requested page could not be found."
+        : error.statusText || details;
+  } else if (import.meta.env.DEV && error && error instanceof Error) {
+    details = error.message;
+    stack = error.stack;
+  }
+
+  return (
+    <main className="pt-16 p-4 container mx-auto">
+      <h1>{message}</h1>
+      <p>{details}</p>
+      {stack && (
+        <pre className="w-full p-4 overflow-x-auto">
+          <code>{stack}</code>
+        </pre>
+      )}
+    </main>
+  );
+}
--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, route, index } from "@react-router/dev/routes";
 export default [
     index("./routes/home.tsx"),
     route("profile", "./routes/profile.tsx"),
-];
+] satisfies RouteConfig;
 
 
