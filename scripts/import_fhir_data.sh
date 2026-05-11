7746c8c628156c1b2f2cbcbdb8d68a709474dcbb
Melanie Xia <melaniekxia@gmail.com>
2026-05-11 15:48:57 -0400

    update paths

--- scripts/import_fhir_data.sh
+++ scripts/import_fhir_data.sh
@@ -1,5 +1,5 @@
 FHIR_URL="http://localhost:8080/fhir"
-DATA_DIR="../data/hl7_fhir_r4_json"
+DATA_DIR="../data/hl7_fhir_r4_output/fhir"
 
 # Upload practitioner information
 echo "Uploading practitioner and hospital information"
@@ -8,9 +8,10 @@ curl -X POST "$FHIR_URL" -H "Content-Type: application/fhir+json" --data @"$DATA
 echo "HospitalInformation and PractitionerInformation import completed"
 
 for file in $DATA_DIR/*.json; do 
-    if [[ "$file" == *practitionerInformation* ]]; then
+    if [[ "$file" == *hospitalInformation* || "$file" == *practitionerInformation* ]]; then
         continue
     fi
+
 
     echo "Uploading $resource_type"
     curl -X POST "$FHIR_URL" -H "Content-Type: application/fhir+json" --data @"$file"
