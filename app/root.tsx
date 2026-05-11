238a3e846764d15ad72ce1162db9be3f54f55d64
Melanie Xia <melaniekxia@gmail.com>
2026-05-11 15:49:53 -0400

    fetch profile info from hapi fhir jpa server

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 import type { Route } from "./+types/root";
-// import "./app.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // export const links: Route.LinksFunction = () => [
 //   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -40,9 +40,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+const queryClient = new QueryClient();
 export default function App() {
   return (
-    <Outlet />
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
 
   );
 }
--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,5 +1,50 @@
+import { useSearchParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+
+
 export default function Profile() {
+  const [params] = useSearchParams();
+  const givenName = params.get("givenName");
+  const familyName = params.get("familyName");
+  const birthDate = params.get("birthDate");  
+
+  const url = new URL("http://localhost:8080/fhir/Patient");
+    
+  if (givenName && familyName && birthDate) {
+    url.searchParams.append("given", givenName);
+    url.searchParams.append("family", familyName);
+    url.searchParams.append("birthdate", birthDate);
+  }
+
+  const {isPending, error, data, isFetching} = useQuery({
+    queryKey: ['patientData'],
+    queryFn: async() => {
+      const response = await fetch(
+        url.toString(),
+      )
+      return response.json()
+    }
+  })
+
+  console.log(data);
+  const patientResource = data?.entry?.[0]?.resource;
+
+  if (isPending) return <h1> Loading... </h1>
+  if (error) return <h1> An error has occurred: {error.message} </h1>
+  if (isFetching) return <div> {isFetching ? "Updating..." : ""} </div>
+
+
   return (
-      <h1> Patient Profile </h1>
+    <div>
+      <p> Name: {patientResource.name?.[0]?.given?.[0]} {patientResource.name?.[0]?.family} </p>
+      <p> DOB: {patientResource.birthDate} </p>
+      <p> Phone: {patientResource.telecom?.[0]?.value} </p>
+      <p> Address:&nbsp;
+        {patientResource.address?.[0]?.line}&nbsp;
+        {patientResource.address?.[0]?.city},&nbsp;
+        {patientResource.address?.[0]?.state}&nbsp;
+        {patientResource.address?.[0]?.postalCode} 
+      </p>
+    </div>
   );
 }
